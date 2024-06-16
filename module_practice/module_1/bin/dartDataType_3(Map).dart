c28b4bb9e5e70b2a22b11288b909d6aed2cb73f8
mdyasinahmed <yasinahmed7875@gmail.com>
2024-06-16 13:21:20 +0600

    add dart Map practice code

---
+++ module_practice/module_1/bin/dartDataType_3(Map).dart
@@ -0,0 +1,19 @@
+main() {
+  // Dart MAP Datatype
+  /**
+   * Format:
+   *      KEY : VALUE
+   *
+   * Terms:
+   *      KEY = SHOULD BE UNIQUE
+   *      VALUE = NOT UNIQUE
+  **/
+
+  var student1 = {
+    'name' : 'Yasin',
+    'age' : 21,
+    'department' : 'CSE'
+  };
+  print(student1);
+  print(student1['name']);
+}
