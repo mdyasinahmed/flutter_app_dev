c9cf528ae96911dc8faf2ca53bac059306cba28b
mdyasinahmed <yasinahmed7875@gmail.com>
2024-06-16 13:07:45 +0600

    add dart list data type practice code

---
+++ module_practice/module_1/bin/dartDataType_2(List).dart
@@ -0,0 +1,11 @@
+main() {
+  // list with same data type
+  var cityListInBD = ['Dhaka', 'Sylhet', 'Comilla', 'Barisal', 'Rangpur'];
+  print(cityListInBD);
+  // print(cityListInBD[1]);
+  // print(cityListInBD[2]);
+
+  // list with multiple data types
+  var collection = ['222-115-081', 21, 4.00, true];
+  print(collection);
+}
