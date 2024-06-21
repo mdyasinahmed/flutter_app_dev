0bee64fd7a52f1edb9d7b3ec6c5a1fec6b4e72db
mdyasinahmed <yasinahmed7875@gmail.com>
2024-06-21 11:46:33 +0600

    add dart constant practice code

---
+++ module_practice/module_1/bin/dartConstant.dart
@@ -0,0 +1,16 @@
+void main() {
+  // dart final and constant
+  // - cannot change during execution of the program
+  // - after initialising cannot reassigned later
+
+
+  final a = 10;
+  print(a);
+  // a = 20; it will show error
+
+  // const a = 10;  it will show error
+  const b = 10;
+  print(b);
+
+
+}
