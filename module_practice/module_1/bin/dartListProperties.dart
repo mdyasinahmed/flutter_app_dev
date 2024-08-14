60521c84d6794fd5b7f12e725cc940fadded2820
mdyasinahmed <yasinahmed7875@gmail.com>
2024-08-14 21:57:28 +0600

    add module 7 practice code files

---
+++ module_practice/module_1/bin/dartListProperties.dart
@@ -0,0 +1,24 @@
+void main() {
+  var bdCity = ['Sylhet', 'Comilla', 'Dhaka', 'Rajshahi', 'Khulna', 'Rangpur', 'Chittagong'];
+
+  // DART LIST PROPERTIES
+  // first
+  var result = bdCity.first;
+  print(result);
+
+  // inEmpty
+  print(bdCity.isEmpty);
+
+  // length
+  print(bdCity.length);
+
+  // last
+  print(bdCity.last);
+
+  // reverse
+  print(bdCity.reversed);
+
+  // single =  check whether it contains only one element or not
+  print(bdCity.singleOrNull);
+
+}
---
+++ module_practice/module_2/bin/dartSET.dart
@@ -0,0 +1,11 @@
+import 'dart:ffi';
+
+main() {
+  var myCitySet = <String>{'Dhaka','Barisal', 'CoxsBazar'};
+  print(myCitySet);
+
+  // var mySet1 = <Int>{100,200, 300};
+  // print(mySet1);
+
+
+}
