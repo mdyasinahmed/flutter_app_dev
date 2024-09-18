351c18e790a815cf8ecdfafcd7a4fab1f2772589
mdyasinahmed <yasinahmed7875@gmail.com>
2024-09-18 07:13:24 +0600

    add flutter assignment created project

---
+++ module_practice/module_14/assignmnet/complete_crud/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:complete_crud/screens/home_screen.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const HomeScreen();
+  }
+}
+
